1029af4528c329a311786989cf4045696459bfab
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-09-23 20:58:26 -0600

    complete personal assignment

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const routes = require("express").Router();
 const baseController = require("../controllers");
 
 routes.get("/", baseController.getName);
+routes.use("/contacts", require("./contacts"));
 
 module.exports = routes;
